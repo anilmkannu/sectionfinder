e55dad228621f9f50db7c6a959a41eb1fd7b52f7
Sushil Yadav <sky21088@gmail.com>
2020-12-29 23:23:20 +0530

    image path

--- sectionfinderSERVER/controllers/srcElement.controller.js
+++ sectionfinderSERVER/controllers/srcElement.controller.js
@@ -30,7 +30,8 @@ const CreateSecElement = require("../models/secElements");
             categoryName: metaSchema.categoryName,
             url: metaSchema.url,
             websiteName: metaSchema.websiteName,
-            image : imageUrl + '/public/uploads/' + req.file.filename,
+           // image : imageUrl + '/public/uploads/' + req.file.filename,
+            image : '/public/uploads/' + req.file.filename,
             title : metaSchema.websiteName + 'by' + metaSchema.categoryName
           }).save()
          // let dataObj = {};  
