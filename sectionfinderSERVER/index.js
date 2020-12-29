46db2078b6af75df02ca9da8382f6b85638932b4
Sushil Yadav <sky21088@gmail.com>
2020-12-30 01:35:23 +0530

    Update index.js

--- sectionfinderSERVER/index.js
+++ sectionfinderSERVER/index.js
@@ -8,14 +8,16 @@ const app = express();
 app.use(cors({ origin: "http://localhost:4200"}));
 app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(express.static(__dirname+"./public"))
+// app.use(express.static(__dirname+"./public"))
+app.use(express.static('public'))
+
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
     res.setHeader('Access-Control-Allow-Methods', 'POST');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
     next();
-});
+    });
 
 app.use(require('./router'));
 
